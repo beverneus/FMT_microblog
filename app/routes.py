5b45d956311b109ada8d1fd2ca6a7a6bba1e07f2
beverneus <beverneus@users.noreply.github.com>
2020-09-21 20:11:54 +0200

    added database

--- app/routes.py
+++ app/routes.py
@@ -26,6 +26,8 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+        flash(
+            f'Login requested for user {form.username.data}, '
+            f'remember_me={form.remember_me.data}')
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
